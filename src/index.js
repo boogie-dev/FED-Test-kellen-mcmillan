d915c91130beac9a4e287c84dcd7ae23e8aacd49
Kellen McMillan <kellen.mcmillan.dev@gmail.com>
2022-02-01 13:47:00 -0600

    preparing first deploy to githup pages

--- src/index.js
+++ src/index.js
@@ -1,2 +1,27 @@
 import 'normalize.css'
 import './styles/app.scss'
+import './styles/kellen.css'
+import './js/kellen.js'
+import data from './data.json';
+import image from 'src/images/logo.png'
+
+
+
+
+function component() {
+	const element = document.createElement('div');
+
+	// Lodash, now imported by this script
+	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+	element.classList.add('hello');
+
+	// Add the image to our existing div.
+	const logo = new Image();
+	logo.src = logo;
+
+	element.appendChild(logo);
+
+	console.log(Data);
+
+	return element;
+}
