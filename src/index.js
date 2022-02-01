7b12f2a1443645e8fff9d21da2468b82ea26c22f
Kellen McMillan <kellen.mcmillan.dev@gmail.com>
2022-02-01 14:22:52 -0600

    preparing second deploy to githup pages

--- src/index.js
+++ src/index.js
@@ -6,7 +6,39 @@ import data from './data.json';
 import image from 'src/images/logo.png'
 
 
+const path = require('path');
 
+module.exports = {
+	entry: './src/index.js',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+	},
+},
+module: {
+	rules: [
+		{
+			test: /\.css$/i,
+			use: ['style-loader', 'css-loader'],
+		},
+		{
+			test: /\.(png|svg|jpg|jpeg|gif)$/i,
+			type: 'asset/resource',
+		},
+		{
+			test: /\.(woff|woff2|eot|ttf|otf)$/i,
+			type: 'asset/resource',
+		},
+		{
+			test: /\.(csv|tsv)$/i,
+			use: ['csv-loader'],
+		},
+		{
+			test: /\.xml$/i,
+			use: ['xml-loader'],
+		},
+	],		
+},
 
 function component() {
 	const element = document.createElement('div');
