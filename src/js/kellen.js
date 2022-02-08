89c10b3e126c5f8fa9c6048dcf9c6981d4a4bd47
Kellen McMillan <kellen.mcmillan.dev@gmail.com>
2022-02-08 14:44:50 -0600

    minor fix

--- src/js/kellen.js
+++ src/js/kellen.js
@@ -26,12 +26,12 @@ function filterize(e) {
 }
 function filterThis(identifier) {
   if (identifier === 'hash') {
-    ;[].map.call(document.querySelectorAll('ul:not(.hidden'), (element) => {
+    ;[].map.call(document.querySelectorAll('ul.directory:not(.hidden'), (element) => {
       element.classList.remove('tempHidden')
     })
   } else {
     const ID = identifier.toUpperCase()
-    ;[].map.call(document.querySelectorAll('ul:not(.hidden'), (element) => {
+    ;[].map.call(document.querySelectorAll('ul.directory:not(.hidden'), (element) => {
       console.log('element', ID, element.getAttribute('id'))
       if (element.getAttribute('id') !== `content${ID}`) {
         element.classList.add('tempHidden')
