2ffc6d6ab6d9fb26f0ec5de6716c48c2521cc7fc
Kellen McMillan <kellen.mcmillan.dev@gmail.com>
2022-02-08 13:02:11 -0600

    drying up code

--- src/js/kellen.js
+++ src/js/kellen.js
@@ -1,318 +1,166 @@
-const switchAndPrint = (item) => {
-  const ele = item.title
+const dryUp = (element, id) => {
   const node = document.createElement('li')
   const anchor = document.createElement('a')
+  const listParent = document.getElementById(`content${element[0]}`)
+  const alphabetLetter = document.getElementById(id)
+  listParent.classList.remove('hidden')
+  alphabetLetter.classList.remove('greyedOut')
+  node.innerText = element
+  anchor.setAttribute('href', '#')
+  anchor.setAttribute('class', 'nostyle')
+  listParent.appendChild(anchor)
+  anchor.appendChild(node)
+}
+function filterize(e) {
+  console.log('attribute', e, typeof e)
+  if (e === 'hash') {
+    if (document.querySelector('ul.shown')) {
+      document.querySelector('ul.shown').classList.remove('shown')
+    }
+  } else {
+    if (document.querySelector('ul.shown')) {
+      document.querySelector('ul.shown').classList.remove('shown')
+    }
+    console.log('id', `#content${e.toUpperCase()}`)
+    document.querySelector(`#content${e.toUpperCase()}`).classList.add('shown')
+  }
+}
+const attachListerner = (id) => {
+  document.getElementById(id).onclick = () => {
+    filterize(id)
+  }
+}
+const switchAndPrint = (item) => {
+  const ele = item.title
   switch (ele.length !== 0) {
     case ele[0] === 'A': {
-      const listParent = document.getElementById('contentA')
-      const alphabetLetter = document.getElementById('a')
-      listParent.classList.remove('hidden')
-      alphabetLetter.classList.remove('greyedOut')
-      node.innerText = ele
-      anchor.setAttribute('href', '#')
-      anchor.setAttribute('class', 'nostyle')
-      listParent.appendChild(anchor)
-      anchor.appendChild(node)
+      dryUp(ele, ele[0].toLowerCase())
+      attachListerner(ele[0].toLowerCase())
       break
     }
     case ele[0] === 'B': {
-      const listParent = document.getElementById('contentB')
-      const alphabetLetter = document.getElementById('b')
-      listParent.classList.remove('hidden')
-      alphabetLetter.classList.remove('greyedOut')
-      node.innerText = ele
-      anchor.setAttribute('href', '#')
-      anchor.setAttribute('class', 'nostyle')
-      listParent.appendChild(anchor)
-      anchor.appendChild(node)
+      dryUp(ele, ele[0].toLowerCase())
+      attachListerner(ele[0].toLowerCase())
       break
     }
     case ele[0] === 'C': {
-      const listParent = document.getElementById('contentC')
-      const alphabetLetter = document.getElementById('c')
-      listParent.classList.remove('hidden')
-      alphabetLetter.classList.remove('greyedOut')
-      node.innerText = ele
-      anchor.setAttribute('href', '#')
-      anchor.setAttribute('class', 'nostyle')
-      listParent.appendChild(anchor)
-      anchor.appendChild(node)
+      dryUp(ele, ele[0].toLowerCase())
+      attachListerner(ele[0].toLowerCase())
       break
     }
     case ele[0] === 'D': {
-      const listParent = document.getElementById('contentD')
-      const alphabetLetter = document.getElementById('d')
-      listParent.classList.remove('hidden')
-      alphabetLetter.classList.remove('greyedOut')
-      node.innerText = ele
-      anchor.setAttribute('href', '#')
-      anchor.setAttribute('class', 'nostyle')
-      listParent.appendChild(anchor)
-      anchor.appendChild(node)
+      dryUp(ele, ele[0].toLowerCase())
+      attachListerner(ele[0].toLowerCase())
       break
     }
     case ele[0] === 'E': {
-      const listParent = document.getElementById('contentE')
-      const alphabetLetter = document.getElementById('e')
-      listParent.classList.remove('hidden')
-      alphabetLetter.classList.remove('greyedOut')
-      node.innerText = ele
-      anchor.setAttribute('href', '#')
-      anchor.setAttribute('class', 'nostyle')
-      listParent.appendChild(anchor)
-      anchor.appendChild(node)
+      dryUp(ele, ele[0].toLowerCase())
+      attachListerner(ele[0].toLowerCase())
       break
     }
     case ele[0] === 'F': {
-      const listParent = document.getElementById('contentF')
-      const alphabetLetter = document.getElementById('f')
-      listParent.classList.remove('hidden')
-      alphabetLetter.classList.remove('greyedOut')
-      node.innerText = ele
-      anchor.setAttribute('href', '#')
-      anchor.setAttribute('class', 'nostyle')
-      listParent.appendChild(anchor)
-      anchor.appendChild(node)
+      dryUp(ele, ele[0].toLowerCase())
+      attachListerner(ele[0].toLowerCase())
       break
     }
     case ele[0] === 'G': {
-      const listParent = document.getElementById('contentG')
-      const alphabetLetter = document.getElementById('g')
-      listParent.classList.remove('hidden')
-      alphabetLetter.classList.remove('greyedOut')
-      node.innerText = ele
-      anchor.setAttribute('href', '#')
-      anchor.setAttribute('class', 'nostyle')
-      listParent.appendChild(anchor)
-      anchor.appendChild(node)
+      dryUp(ele, ele[0].toLowerCase())
+      attachListerner(ele[0].toLowerCase())
       break
     }
     case ele[0] === 'H': {
-      const listParent = document.getElementById('contentH')
-      const alphabetLetter = document.getElementById('h')
-      listParent.classList.remove('hidden')
-      alphabetLetter.classList.remove('greyedOut')
-      node.innerText = ele
-      anchor.setAttribute('href', '#')
-      anchor.setAttribute('class', 'nostyle')
-      listParent.appendChild(anchor)
-      anchor.appendChild(node)
+      dryUp(ele, ele[0].toLowerCase())
+      attachListerner(ele[0].toLowerCase())
       break
     }
     case ele[0] === 'I': {
-      const listParent = document.getElementById('contentI')
-      const alphabetLetter = document.getElementById('i')
-      listParent.classList.remove('hidden')
-      alphabetLetter.classList.remove('greyedOut')
-      node.innerText = ele
-      anchor.setAttribute('href', '#')
-      anchor.setAttribute('class', 'nostyle')
-      listParent.appendChild(anchor)
-      anchor.appendChild(node)
+      dryUp(ele, ele[0].toLowerCase())
+      attachListerner(ele[0].toLowerCase())
       break
     }
     case ele[0] === 'J': {
-      const listParent = document.getElementById('contentJ')
-      const alphabetLetter = document.getElementById('j')
-      listParent.classList.remove('hidden')
-      alphabetLetter.classList.remove('greyedOut')
-      node.innerText = ele
-      anchor.setAttribute('href', '#')
-      anchor.setAttribute('class', 'nostyle')
-      listParent.appendChild(anchor)
-      anchor.appendChild(node)
+      dryUp(ele, ele[0].toLowerCase())
+      attachListerner(ele[0].toLowerCase())
       break
     }
     case ele[0] === 'K': {
-      const listParent = document.getElementById('contentK')
-      const alphabetLetter = document.getElementById('k')
-      listParent.classList.remove('hidden')
-      alphabetLetter.classList.remove('greyedOut')
-      node.innerText = ele
-      anchor.setAttribute('href', '#')
-      anchor.setAttribute('class', 'nostyle')
-      listParent.appendChild(anchor)
-      anchor.appendChild(node)
+      dryUp(ele, ele[0].toLowerCase())
+      attachListerner(ele[0].toLowerCase())
       break
     }
     case ele[0] === 'L': {
-      const listParent = document.getElementById('contentL')
-      const alphabetLetter = document.getElementById('l')
-      listParent.classList.remove('hidden')
-      alphabetLetter.classList.remove('greyedOut')
-      node.innerText = ele
-      anchor.setAttribute('href', '#')
-      anchor.setAttribute('class', 'nostyle')
-      listParent.appendChild(anchor)
-      anchor.appendChild(node)
+      dryUp(ele, ele[0].toLowerCase())
+      attachListerner(ele[0].toLowerCase())
       break
     }
     case ele[0] === 'M': {
-      const listParent = document.getElementById('contentM')
-      const alphabetLetter = document.getElementById('m')
-      listParent.classList.remove('hidden')
-      alphabetLetter.classList.remove('greyedOut')
-      node.innerText = ele
-      anchor.setAttribute('href', '#')
-      anchor.setAttribute('class', 'nostyle')
-      listParent.appendChild(anchor)
-      anchor.appendChild(node)
+      dryUp(ele, ele[0].toLowerCase())
+      attachListerner(ele[0].toLowerCase())
       break
     }
     case ele[0] === 'N': {
-      const listParent = document.getElementById('contentN')
-      const alphabetLetter = document.getElementById('n')
-      listParent.classList.remove('hidden')
-      alphabetLetter.classList.remove('greyedOut')
-      node.innerText = ele
-      anchor.setAttribute('href', '#')
-      anchor.setAttribute('class', 'nostyle')
-      listParent.appendChild(anchor)
-      anchor.appendChild(node)
+      dryUp(ele, ele[0].toLowerCase())
+      attachListerner(ele[0].toLowerCase())
       break
     }
     case ele[0] === 'O': {
-      const listParent = document.getElementById('contentO')
-      const alphabetLetter = document.getElementById('o')
-      listParent.classList.remove('hidden')
-      alphabetLetter.classList.remove('greyedOut')
-      node.innerText = ele
-      anchor.setAttribute('href', '#')
-      anchor.setAttribute('class', 'nostyle')
-      listParent.appendChild(anchor)
-      anchor.appendChild(node)
+      dryUp(ele, ele[0].toLowerCase())
+      attachListerner(ele[0].toLowerCase())
       break
     }
     case ele[0] === 'P': {
-      const listParent = document.getElementById('contentP')
-      const alphabetLetter = document.getElementById('p')
-      listParent.classList.remove('hidden')
-      alphabetLetter.classList.remove('greyedOut')
-      node.innerText = ele
-      anchor.setAttribute('href', '#')
-      anchor.setAttribute('class', 'nostyle')
-      listParent.appendChild(anchor)
-      anchor.appendChild(node)
+      dryUp(ele, ele[0].toLowerCase())
+      attachListerner(ele[0].toLowerCase())
       break
     }
     case ele[0] === 'Q': {
-      const listParent = document.getElementById('contentQ')
-      const alphabetLetter = document.getElementById('q')
-      listParent.classList.remove('hidden')
-      alphabetLetter.classList.remove('greyedOut')
-      node.innerText = ele
-      anchor.setAttribute('href', '#')
-      anchor.setAttribute('class', 'nostyle')
-      listParent.appendChild(anchor)
-      anchor.appendChild(node)
+      dryUp(ele, ele[0].toLowerCase())
+      attachListerner(ele[0].toLowerCase())
       break
     }
     case ele[0] === 'R': {
-      const listParent = document.getElementById('contentR')
-      const alphabetLetter = document.getElementById('r')
-      listParent.classList.remove('hidden')
-      alphabetLetter.classList.remove('greyedOut')
-      node.innerText = ele
-      anchor.setAttribute('href', '#')
-      anchor.setAttribute('class', 'nostyle')
-      listParent.appendChild(anchor)
-      anchor.appendChild(node)
+      dryUp(ele, ele[0].toLowerCase())
+      attachListerner(ele[0].toLowerCase())
       break
     }
     case ele[0] === 'S': {
-      const listParent = document.getElementById('contentS')
-      const alphabetLetter = document.getElementById('s')
-      listParent.classList.remove('hidden')
-      alphabetLetter.classList.remove('greyedOut')
-      node.innerText = ele
-      anchor.setAttribute('href', '#')
-      anchor.setAttribute('class', 'nostyle')
-      listParent.appendChild(anchor)
-      anchor.appendChild(node)
+      dryUp(ele, ele[0].toLowerCase())
+      attachListerner(ele[0].toLowerCase())
       break
     }
     case ele[0] === 'T': {
-      const listParent = document.getElementById('contentT')
-      const alphabetLetter = document.getElementById('t')
-      listParent.classList.remove('hidden')
-      alphabetLetter.classList.remove('greyedOut')
-      node.innerText = ele
-      anchor.setAttribute('href', '#')
-      anchor.setAttribute('class', 'nostyle')
-      listParent.appendChild(anchor)
-      anchor.appendChild(node)
+      dryUp(ele, ele[0].toLowerCase())
+      attachListerner(ele[0].toLowerCase())
       break
     }
     case ele[0] === 'U': {
-      const listParent = document.getElementById('contentU')
-      const alphabetLetter = document.getElementById('u')
-      listParent.classList.remove('hidden')
-      alphabetLetter.classList.remove('greyedOut')
-      node.innerText = ele
-      anchor.setAttribute('href', '#')
-      anchor.setAttribute('class', 'nostyle')
-      listParent.appendChild(anchor)
-      anchor.appendChild(node)
+      dryUp(ele, ele[0].toLowerCase())
+      attachListerner(ele[0].toLowerCase())
       break
     }
     case ele[0] === 'V': {
-      const listParent = document.getElementById('contentV')
-      const alphabetLetter = document.getElementById('v')
-      listParent.classList.remove('hidden')
-      alphabetLetter.classList.remove('greyedOut')
-      node.innerText = ele
-      anchor.setAttribute('href', '#')
-      anchor.setAttribute('class', 'nostyle')
-      listParent.appendChild(anchor)
-      anchor.appendChild(node)
+      dryUp(ele, ele[0].toLowerCase())
+      attachListerner(ele[0].toLowerCase())
       break
     }
     case ele[0] === 'W': {
-      const listParent = document.getElementById('contentW')
-      const alphabetLetter = document.getElementById('w')
-      listParent.classList.remove('hidden')
-      alphabetLetter.classList.remove('greyedOut')
-      node.innerText = ele
-      anchor.setAttribute('href', '#')
-      anchor.setAttribute('class', 'nostyle')
-      listParent.appendChild(anchor)
-      anchor.appendChild(node)
+      dryUp(ele, ele[0].toLowerCase())
+      attachListerner(ele[0].toLowerCase())
       break
     }
     case ele[0] === 'X': {
-      const listParent = document.getElementById('contentX')
-      const alphabetLetter = document.getElementById('x')
-      listParent.classList.remove('hidden')
-      alphabetLetter.classList.remove('greyedOut')
-      node.innerText = ele
-      anchor.setAttribute('href', '#')
-      anchor.setAttribute('class', 'nostyle')
-      listParent.appendChild(anchor)
-      anchor.appendChild(node)
+      dryUp(ele, ele[0].toLowerCase())
+      attachListerner(ele[0].toLowerCase())
       break
     }
     case ele[0] === 'Y': {
-      const listParent = document.getElementById('contentY')
-      const alphabetLetter = document.getElementById('y')
-      listParent.classList.remove('hidden')
-      alphabetLetter.classList.remove('greyedOut')
-      node.innerText = ele
-      anchor.setAttribute('href', '#')
-      anchor.setAttribute('class', 'nostyle')
-      listParent.appendChild(anchor)
-      anchor.appendChild(node)
+      dryUp(ele, ele[0].toLowerCase())
+      attachListerner(ele[0].toLowerCase())
       break
     }
     case ele[0] === 'Z': {
-      const listParent = document.getElementById('contentZ')
-      const alphabetLetter = document.getElementById('z')
-      listParent.classList.remove('hidden')
-      alphabetLetter.classList.remove('greyedOut')
-      node.innerText = ele
-      anchor.setAttribute('href', '#')
-      anchor.setAttribute('class', 'nostyle')
-      listParent.appendChild(anchor)
-      anchor.appendChild(node)
+      dryUp(ele, ele[0].toLowerCase())
+      attachListerner(ele[0].toLowerCase())
       break
     }
     default: {
@@ -339,99 +187,3 @@ fetch('./assets/data.json')
     )
     return true
   })
-function filterize(e) {
-  console.log('attribute', e, typeof e)
-  if (e === 'hash') {
-    if (document.querySelector('ul.shown')) {
-      document.querySelector('ul.shown').classList.remove('shown')
-    }
-  } else {
-    if (document.querySelector('ul.shown')) {
-      document.querySelector('ul.shown').classList.remove('shown')
-    }
-    console.log('id', `#content${e.toUpperCase()}`)
-    document.querySelector(`#content${e.toUpperCase()}`).classList.add('shown')
-  }
-}
-
-document.getElementById('a').onclick = () => {
-  return filterize('a')
-}
-document.getElementById('b').onclick = () => {
-  return filterize('b')
-}
-document.getElementById('c').onclick = () => {
-  return filterize('c')
-}
-document.getElementById('d').onclick = () => {
-  return filterize('d')
-}
-document.getElementById('e').onclick = () => {
-  return filterize('e')
-}
-document.getElementById('f').onclick = () => {
-  return filterize('f')
-}
-document.getElementById('g').onclick = () => {
-  return filterize('g')
-}
-document.getElementById('h').onclick = () => {
-  return filterize('h')
-}
-document.getElementById('i').onclick = () => {
-  return filterize('i')
-}
-document.getElementById('j').onclick = () => {
-  return filterize('j')
-}
-document.getElementById('k').onclick = () => {
-  return filterize('k')
-}
-document.getElementById('l').onclick = () => {
-  return filterize('l')
-}
-document.getElementById('m').onclick = () => {
-  return filterize('m')
-}
-document.getElementById('n').onclick = () => {
-  return filterize('n')
-}
-document.getElementById('o').onclick = () => {
-  return filterize('o')
-}
-document.getElementById('p').onclick = () => {
-  return filterize('p')
-}
-document.getElementById('q').onclick = () => {
-  return filterize('q')
-}
-document.getElementById('r').onclick = () => {
-  return filterize('r')
-}
-document.getElementById('s').onclick = () => {
-  return filterize('s')
-}
-document.getElementById('t').onclick = () => {
-  return filterize('t')
-}
-document.getElementById('u').onclick = () => {
-  return filterize('u')
-}
-document.getElementById('v').onclick = () => {
-  return filterize('v')
-}
-document.getElementById('w').onclick = () => {
-  return filterize('w')
-}
-document.getElementById('x').onclick = () => {
-  return filterize('x')
-}
-document.getElementById('y').onclick = () => {
-  return filterize('y')
-}
-document.getElementById('z').onclick = () => {
-  return filterize('z')
-}
-document.getElementById('hash').onclick = () => {
-  return filterize('hash')
-}
